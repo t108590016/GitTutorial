218c4d7d8906fb919ef429bbfd591f79ee633431
t108590002 <t108590002@ntut.org.tw>
2021-03-12 01:05:11 +0800

    8B

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -9,13 +9,15 @@
 bool Guess(int number) {
 	static int target = -1;
 	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
 	if (number == target) {
 		std::cout << "Correct !!";
 		target = -1;
 		return true;
 	}
-	else if (number > target) std::cout << "Smaller" << std::endl;
-	else if (number < target) std::cout << "Bigger" << std::endl;
+	else std::cout << "Wrong" << std::endl;
 	return false;
 
 }
