7ea54ea5585f34cbafc90ad2ed71d11fb1e59a05
DESKTOP-OM0C3VM\lynn4 <t108590016@ntut.org.tw>
2021-03-12 01:06:10 +0800

    8A

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -7,21 +7,7 @@
 
 
 bool Guess(int number) {
-	static int target = -1;
-
-	srand(time(NULL));
-	if (target == -1) {
-		target = rand() % 100 + 1;
-	}
-	if (number > target) {
-		std::cout << "Smaller" << std::endl;
-		return false;
-	}
-	else if (number < target) {
-		std::cout << "Bigger" << std::endl;
-		return false;
-	}
-	return true;
+	return false;
 }
 int main()
 {
